48e94a3fb20a23eb5165e24f76b55b7e5648e909
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-10-13 14:09:15 +0800

    fix: typo MINIO_ACCESSLEY, KEY not LEY, and format

--- backend/workspace/nestjs/src/modules/file/minio/minio.service.ts
+++ backend/workspace/nestjs/src/modules/file/minio/minio.service.ts
@@ -12,42 +12,41 @@ export class MinioService {
     this.minioClient = new Minio.Client({
       endPoint: process.env.MINIO_ENDPOINT,
       port: parseInt(process.env.MINIO_PORT),
-      accessKey: process.env.MINIO_ACCESSLEY,
+      accessKey: process.env.MINIO_ACCESSKEY,
       secretKey: process.env.MINIO_SECRETKEY,
       useSSL: false,
     });
   }
 
   //save the file to MinIO
-  async uploadFile(bucket: string, location: string, file: Express.MulterFile, newfilename?: string) {
-    let filename =""
-    if (newfilename == undefined || newfilename.trim() == "")
-    {
+  async uploadFile(
+    bucket: string,
+    location: string,
+    file: Express.MulterFile,
+    newfilename?: string,
+  ) {
+    let filename = '';
+    if (newfilename == undefined || newfilename.trim() == '') {
       filename = path.join(location, file.originalname);
-    }
-    else
-    {
+    } else {
       filename = path.join(location, newfilename);
     }
     filename = UpdateFilePath(filename);
-    
+
     console.log(`Uploading file: ${filename} to bucket: ${bucket}`);
-    
+
     return await this.minioClient.putObject(bucket, filename, file.buffer);
   }
-  
+
   //get file from MinIO
   async downloadFile(bucket: string, location: string, filename: string) {
-    
-    let filefullpath = UpdateFilePath(path.join(location,filename))
+    const filefullpath = UpdateFilePath(path.join(location, filename));
     console.log(`Downloading file: ${filefullpath} from bucket: ${bucket}`);
     return await this.getObject(bucket, filefullpath);
-    
   }
 
   //get object from Bucket
-  async getObject(bucket, filefullpath)
-  {
+  async getObject(bucket, filefullpath) {
     return await this.minioClient.getObject(bucket, filefullpath);
   }
   //check bucket exists or not, create if bucket does not exists
@@ -61,25 +60,26 @@ export class MinioService {
   }
 
   //check bucket exists
-  async checkBucketExist(bucketName: string): Promise<boolean>
-  {
+  async checkBucketExist(bucketName: string): Promise<boolean> {
     const bucketExists = await this.minioClient.bucketExists(bucketName);
     if (!bucketExists) {
-      return false
-    }
-    else {
-      return true
+      return false;
+    } else {
+      return true;
     }
   }
 
   //list all object under specific directory
-  async listObjects(bucketName: string, prefix: string): Promise<Minio.BucketItem[]> {
+  async listObjects(
+    bucketName: string,
+    prefix: string,
+  ): Promise<Minio.BucketItem[]> {
     const objects: Minio.BucketItem[] = [];
     const stream = this.minioClient.listObjectsV2(bucketName, prefix, true);
     return new Promise((resolve, reject) => {
-      stream.on('data', obj => objects.push(obj));
+      stream.on('data', (obj) => objects.push(obj));
       stream.on('end', () => resolve(objects));
-      stream.on('error', err => reject(err));
+      stream.on('error', (err) => reject(err));
     });
   }
 }
