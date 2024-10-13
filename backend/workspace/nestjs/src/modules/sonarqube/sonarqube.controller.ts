8fcf7eb336cd58a2e94e570e977f4a1bb1aa4087
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-10-14 01:09:31 +0800

    feat: backend, init sonarqube client (not working)

--- backend/workspace/nestjs/src/modules/sonarqube/sonarqube.controller.ts
+++ backend/workspace/nestjs/src/modules/sonarqube/sonarqube.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  BadRequestException,
+} from '@nestjs/common';
 import { SonarqubeService } from './sonarqube.service';
 import { UserService } from '../user/user.service';
 
@@ -12,6 +18,25 @@ export class SonarqubeController {
   @Get()
   async findAll() {
     return this.userService.findAll();
+  }
+
+  @Get('health')
+  async health() {
+    const health = await this.sonarqubeService.isHealth();
+    console.log('health:', health);
+    return health;
+  }
+
+  @Post('createProject')
+  async createProject(
+    @Body('projectKey') projectKey: string,
+    @Body('projectName') projectName: string,
+  ) {
+    console.log(projectKey, projectName);
+    if (!projectKey || !projectName) {
+      throw new BadRequestException('Project key and name are required');
+    }
+    return this.sonarqubeService.createProject(projectKey, projectName);
   }
 
   @Get('helloWorld')
--- backend/workspace/nestjs/src/modules/sonarqube/sonarqube.service.ts
+++ backend/workspace/nestjs/src/modules/sonarqube/sonarqube.service.ts
@@ -1,8 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
 
 @Injectable()
 export class SonarqubeService {
+  sonarqubeUrl: string;
+  sonarqubeToken: string;
+  client: AxiosInstance;
+
+  constructor() {
+    this.sonarqubeUrl = process.env.SONARQUBE_URL;
+    this.sonarqubeToken = process.env.SONARQUBE_TOKEN;
+    console.log(
+      'Init SonarqubeService',
+      this.sonarqubeUrl,
+      this.sonarqubeToken,
+    );
+    this.client = axios.create({
+      baseURL: this.sonarqubeUrl,
+      headers: {
+        Authorization: `Bearer ${this.sonarqubeToken}`,
+      },
+    });
+  }
+
   async helloWorld() {
     return 'Hello World';
+  }
+
+  async isHealth() {
+    try {
+      const response = await this.client.get('/api/system/health');
+      console.log('response:', response);
+      return response;
+    } catch (error) {
+      console.error('Error checking health:', error.message);
+      throw error;
+    }
+  }
+
+  async createProject(projectKey: string, projectName: string) {
+    try {
+      const url = `${this.sonarqubeUrl}/api/projects/create`;
+      const response = await axios.post(
+        url,
+        {
+          project: projectKey,
+          name: projectName,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.sonarqubeToken}`,
+          },
+        },
+      );
+      // Consider logging only in development environment
+      console.log('Project created:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error creating project:', error.message);
+      throw error; // Re-throw the error for the caller to handle
+    }
   }
 }
