bab8ad38a53e12042aded6331d57b87ea499981a
Albert Tse <cktse36.study@gmail.com>
2024-10-21 01:43:00 +0800

    add function to ollama

--- backend/workspace/nestjs/src/modules/ollama/ollama.controller.ts
+++ backend/workspace/nestjs/src/modules/ollama/ollama.controller.ts
@@ -11,6 +11,28 @@ export class OllamaController {
   @HttpCode(HttpStatus.OK)
   @Post('call')
   async callLlama(@Body('prompt') prompt: string) {
-    return this.ollamaService.callLlama(prompt);
+    var result = await this.ollamaService.callLlama(prompt);
+    var choices = result['choices'];
+    console.log(choices[0]['text']);
+    return result;
   }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('callCodeQwen')
+  async callForCodeQwen(@Body('prompt') prompt: string) {
+    return this.ollamaService.callForCodeInfill(prompt, 'qwen');
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('callForUnitTestOrCodeReview')
+  async callForUnitTestOrCodeReview(@Body('prompt') prompt: string) {
+    return this.ollamaService.callForUnitTestOrCodeReview(prompt, "code_review");
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('callCode7bcode')
+  async callForCodeInfill(@Body('prompt') prompt: string) {
+    return this.ollamaService.callForCodeInfill(prompt, 'codeLlama');
+  }
+  
 }
--- backend/workspace/nestjs/src/modules/ollama/ollama.service.ts
+++ backend/workspace/nestjs/src/modules/ollama/ollama.service.ts
@@ -1,9 +1,25 @@
+//https://ollama.com/blog/how-to-prompt-code-llama
+//https://ollama.com/library/qwen2.5-coder
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+
+export interface FunctionInfo {
+  name: string;
+  parameters: string[];
+  returnType: string | null;
+}
 
 @Injectable()
 export class OllamaService {
   private readonly ollamaUrl = process.env.OLLAMA_ENDPOINT; //http://localhost:11434/v1
+
+  public readonly model_qwen: string = "qwen2.5-coder"
+  public readonly model_codellamaInfill: string = "codellama:7b-code"
+  //for testcase or code review: bug suggestion
+  public readonly model_codellama: string = "codellama"
+  
+  public readonly code_review: string ="code_review"
+  public readonly testcase_suggestion: string ="testcase_suggestion"
 
   constructor(private readonly httpService: HttpService) {}
 
@@ -11,6 +27,75 @@ export class OllamaService {
     const response = await this.httpService
       .post(`${this.ollamaUrl}/completions`, {
         model: process.env.OLLAMA_MODEL, //'llama3.2',//'codellama',
+        prompt: prompt,
+      })
+      .toPromise();
+    return response.data;
+  }
+
+  //to for unit test or code review
+  async callForUnitTestOrCodeReview(code: string, reviewType: string ): Promise<any> {
+    var prompt = ''
+    if (reviewType == this.code_review) {
+      prompt = this.constructCodeReviewPrompt(code)
+    }
+    else {
+      prompt = this.constructCodeReviewPrompt(code)
+    }
+    console.log(prompt);
+    const response = await this.httpService
+      .post(`${this.ollamaUrl}/completions`, {
+        model: this.model_codellama, //'llama3.2',//'codellama',
+        prompt: prompt,
+      })
+      .toPromise();
+    return response.data;
+  }
+
+  async callGivePythonCodeSuggestion(FunctionInfo: FunctionInfo, SelectModule: string): Promise<any> {
+    const prompt = this.constructPythonInfillPrompt(FunctionInfo, SelectModule)
+    console.log(prompt)
+    return this.callForCodeInfill(prompt, SelectModule)
+  }
+
+  constructCodeReviewPrompt(code: string): string {
+    return "Where is the bug in this code? \r\n\r\n " + code
+  }
+
+  constructUnitTestPrompt(code: string): string {
+    return "write a unit test for this function: " + code
+  }
+
+
+  constructPythonInfillPrompt(FunctionInfo: FunctionInfo, SelectModule: string): any {
+    var result =''
+    if (SelectModule == this.model_qwen)
+    {
+      //pending to implement
+    }
+    else{
+      result = '<PRE> def ' + FunctionInfo['name'];
+      result = result + '(' + FunctionInfo['parameters'] + '):' + '<SUF> ';
+      
+      if (FunctionInfo['returnType'] != null)
+          result = result + 'return ' + FunctionInfo['returnType'];
+
+      result = result + ' <MID>'
+    }
+    return result;
+  }
+
+  //for model to perform Code Infill
+  async callForCodeInfill(prompt: string, SelectModule: string): Promise<any> {
+    let model = '';
+    if (SelectModule == this.model_qwen)
+      model = this.model_qwen 
+    else
+      model = this.model_codellamaInfill
+    console.log(model)
+    const response = await this.httpService
+      .post(`${this.ollamaUrl}/completions`, {
+        model: model, //'llama3.2',//'codellama',
         prompt: prompt,
       })
       .toPromise();
