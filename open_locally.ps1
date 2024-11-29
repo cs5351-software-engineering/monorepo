328b3e9fcc92dd6d9df885ea6010875c3d6aa032
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-11-30 01:17:21 +0800

    feat: update open_locally.ps1

--- open_locally.ps1
+++ open_locally.ps1
@@ -1,8 +1,14 @@
 
-& ./backend_postgres/start.ps1
-& ./minio/start.ps1
-& ./sonarqube/up.ps1
-& ./ollama/up.ps1
-
 cursor ./frontend/workspace/nextjs
 cursor ./backend/workspace/nestjs
+
+& ./backend_postgres/start.ps1
+& ./minio/start.ps1
+
+Set-Location ./sonarqube
+& ./up.ps1
+
+Set-Location ../
+
+Set-Location ./ollama
+& ./up.ps1
